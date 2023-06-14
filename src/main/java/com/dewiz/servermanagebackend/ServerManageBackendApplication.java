2c39ef66f74b1b678a6053f73ebed67ff545e07e
dewiz <churkin.it@gmail.com>
2023-06-14 22:10:10 +0300

    Initial commit

---
+++ src/main/java/com/dewiz/servermanagebackend/ServerManageBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dewiz.servermanagebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerManageBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerManageBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dewiz/servermanagebackend/ServerManageBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dewiz.servermanagebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerManageBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
