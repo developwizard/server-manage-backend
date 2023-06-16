6b534aa073e7abcfff23359fcef1e9a87a2e6b1b
dewiz <churkin.it@gmail.com>
2023-06-16 23:42:03 +0300

    Implemented ServerController

---
+++ src/main/java/com/dewiz/servermanagebackend/controller/ServerController.java
@@ -0,0 +1,97 @@
+package com.dewiz.servermanagebackend.controller;
+
+import com.dewiz.servermanagebackend.enumeration.Status;
+import com.dewiz.servermanagebackend.model.Response;
+import com.dewiz.servermanagebackend.model.Server;
+import com.dewiz.servermanagebackend.service.ServerServiceImpl;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.Objects;
+
+import static org.springframework.http.MediaType.IMAGE_PNG_VALUE;
+
+@RestController("/server")
+@RequiredArgsConstructor
+public class ServerController {
+    private final ServerServiceImpl serverService;
+
+    @GetMapping("list")
+    public ResponseEntity<Response> getServers() {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .data(Map.of("servers", serverService.list(30)))
+                        .message("Servers retrieved")
+                        .status(HttpStatus.OK)
+                        .statusCode(HttpStatus.OK.value())
+                        .build()
+        );
+    }
+
+    @GetMapping("ping/{ipAddress}")
+    public ResponseEntity<Response> pingServer(@PathVariable("ipAddress") String ipAddress) throws IOException {
+        Server server = serverService.ping(ipAddress);
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .data(Map.of("server", server))
+                        .message(server.getStatus() == Status.SERVER_UP ? "Ping success" : "Ping failed")
+                        .status(HttpStatus.OK)
+                        .statusCode(HttpStatus.OK.value())
+                        .build()
+        );
+    }
+
+    @PostMapping("save")
+    public ResponseEntity<Response> pingServer(@RequestBody @Valid Server server)  {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .data(Map.of("server", serverService.create(server)))
+                        .message("Server created")
+                        .status(HttpStatus.CREATED)
+                        .statusCode(HttpStatus.CREATED.value())
+                        .build()
+        );
+    }
+
+    @GetMapping("get/{id}")
+    public ResponseEntity<Response> pingServer(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .data(Map.of("server", serverService.get(id)))
+                        .message("Server retrieved")
+                        .status(HttpStatus.OK)
+                        .statusCode(HttpStatus.OK.value())
+                        .build()
+        );
+    }
+
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<Response> deleteServer(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .data(Map.of("deleted", serverService.delete(id)))
+                        .message("Server deleted")
+                        .status(HttpStatus.OK)
+                        .statusCode(HttpStatus.OK.value())
+                        .build()
+        );
+    }
+
+    @GetMapping(path = "image/{fileName}", produces = IMAGE_PNG_VALUE)
+    public byte[] getServerImage(@PathVariable("fileName") String fileName) throws IOException {
+        return Files.readAllBytes(Path.of(Objects.requireNonNull(getClass().getResource(fileName)).getPath()));
+    }
+}
